cf8bee979a1212f9234966f985257cb12853ef70
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-12-20 03:52:07 +0100

    removed unused imports

--- src/main/java/com/dawidfrankiewicz/todo/api/controller/AuthenticationController.java
+++ src/main/java/com/dawidfrankiewicz/todo/api/controller/AuthenticationController.java
@@ -3,26 +3,13 @@ package com.dawidfrankiewicz.todo.api.controller;
 import com.dawidfrankiewicz.todo.api.model.User;
 import com.dawidfrankiewicz.todo.repository.UserRepository;
 
-import jakarta.websocket.server.PathParam;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
--- src/main/java/com/dawidfrankiewicz/todo/configuration/SecurityConfiguration.java
+++ src/main/java/com/dawidfrankiewicz/todo/configuration/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.dawidfrankiewicz.todo.configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
