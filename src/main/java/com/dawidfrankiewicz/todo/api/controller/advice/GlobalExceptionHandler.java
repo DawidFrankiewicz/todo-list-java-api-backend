15ea4fde95a90f6f489491426461f5b2fc33ddd5
GoRDoN <pawel.wielgosz21@gmail.com>
2023-12-30 13:28:03 +0100

    added ResponseEntity

--- src/main/java/com/dawidfrankiewicz/todo/api/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/dawidfrankiewicz/todo/api/controller/advice/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.dawidfrankiewicz.todo.api.controller.advice;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,10 +17,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BindException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleBindExceptionHandler(BindException exception) {
+    public ResponseEntity<String> handleBindExceptionHandler(BindException exception) {
         String errors = exception.getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining("\", \""));
-        String response = "{\"status\":400,\"error\": \"Bad request\",\"message\":[\""+errors+"\"]}";
-        return response;
+        String response = "{\"status\":400,\"error\": \"Bad request\",\"message\":[\"" + errors + "\"]}";
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "application/json");
+        ResponseEntity.ok().headers(responseHeaders);
+        return ResponseEntity.ok().headers(responseHeaders).body(response);
     }
 }
